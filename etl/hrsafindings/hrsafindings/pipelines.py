e69ef92bdcd853bcf044cdda7fc5e777f86c5625
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-05-11 19:49:49 -0700

    Use item pipeline

--- etl/hrsafindings/hrsafindings/pipelines.py
+++ etl/hrsafindings/hrsafindings/pipelines.py
@@ -4,8 +4,15 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+from pathlib import Path
+from .helpers import full_year, entity_abv, closure_date
 
 
 class HrsafindingsPipeline:
     def process_item(self, item, spider):
+        print(Path(__file__))
+        item['full_year'] = full_year(item['year'])
+        item['entity_abv'] = entity_abv(item['id'])
+        item['closure_date'] = closure_date(item['cap_status'])
+        print()
         return item
