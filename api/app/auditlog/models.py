c3e13c0b2b351c75b8ba552eb15c862753a94c7a
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-05-15 07:28:34 -0700

    Update model to include all record fields

--- api/app/auditlog/models.py
+++ api/app/auditlog/models.py
@@ -4,4 +4,14 @@ from app.database import db, Model, Column
 class Record(Model):
     __tablename__ = 'records'
     id = Column(db.Integer, primary_key=True)
+    hrsa_id = Column(db.String(32))
     entity = Column(db.String(32))
+    entity_abv = Column(db.String(32))
+    state = Column(db.String(32))
+    year = Column(db.String(32))
+    full_year = Column(db.Integer)
+    opa_findings = Column(db.String())
+    sanction = Column(db.String())
+    cap_status = Column(db.String())
+    closure_date = Column(db.Date())
+    entity_contact = Column(db.String())
