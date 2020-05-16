777da8c9df7b5e947c858e2b7d3a463da04d60cf
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-05-16 12:30:11 -0700

    Update seed file to populate db based on data file

--- api/app/cli.py
+++ api/app/cli.py
@@ -1,8 +1,30 @@
+import json
+import datetime
 import click
+from pathlib import Path
+from flask.cli import with_appcontext
 from app.auditlog.models import Record
+from app.database import db
 
 
 @click.command()
+@with_appcontext
 @click.argument('filepath', type=click.Path(exists=True))
 def seed(filepath):
-    print(filepath)
+    """Seed database given specific json data file"""
+    f = Path(filepath).resolve()
+    objs = []
+    with f.open() as data:
+        records = json.loads(data.read())
+        for index, record_data in enumerate(records):
+            closure_date = record_data.pop('closure_date')
+            try:
+                closure_date = datetime.datetime.strptime(closure_date, '%B %d, %Y').date()
+            except:
+                closure_date = None
+            objs.append(
+                Record(**record_data, closure_date=closure_date)
+            )
+
+        db.session.add_all(objs)
+        db.session.commit()
