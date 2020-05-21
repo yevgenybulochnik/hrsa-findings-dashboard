c9438b9d089cf2e11c2702a3fc8da62ccb1581ed
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-05-21 08:00:02 -0700

    Marshmallow setup

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_marshmallow import Marshmallow
 from config import Config
 from app.database import db
+
+ma = Marshmallow()
 
 
 def create_app(config=Config):
@@ -8,9 +11,10 @@ def create_app(config=Config):
     app.config.from_object(config)
 
     db.init_app(app)
+    ma.init_app(ma)
 
     from . import auditlog
-    app.register_blueprint(auditlog.bp)
+    app.register_blueprint(auditlog.bp, url_prefix='/api')
 
     with app.app_context():
         db.create_all()
