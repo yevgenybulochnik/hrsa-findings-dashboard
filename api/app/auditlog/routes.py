2bff7da5026def12ab9d097619b9505deaf64cc9
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-05-21 08:16:36 -0700

    Initial routes

--- api/app/auditlog/routes.py
+++ api/app/auditlog/routes.py
@@ -1,6 +1,23 @@
+import json
+from sqlalchemy import func
+from flask import jsonify
+from app.database import db
 from . import bp
 from .models import Record
+from .schemas import record_schema, records_schema
 
-@bp.route('/')
+
+@bp.route("/")
 def index():
-    return 'Hello world'
+    records = Record.query.all()
+    return {"data": records_schema.dumps(records)}
+
+
+@bp.route("/summary")
+def summary():
+    query = (
+        db.session.query(Record.full_year, func.count(Record.id))
+        .group_by(Record.full_year)
+        .all()
+    )
+    return dict(query)
