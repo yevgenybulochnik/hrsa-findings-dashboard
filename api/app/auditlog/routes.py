c2ee1d9739954ec8ec1f19d8cd231df155229249
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-06-01 19:15:36 -0700

    Filter records with webargs

--- api/app/auditlog/routes.py
+++ api/app/auditlog/routes.py
@@ -1,4 +1,7 @@
+from itertools import chain
 import json
+from webargs import fields
+from webargs.flaskparser import use_args, use_kwargs
 from sqlalchemy import func
 from flask import jsonify
 from app.database import db
@@ -7,10 +10,27 @@ from .models import Record
 from .schemas import record_schema, records_schema
 
 
-@bp.route("/")
-def index():
-    records = Record.query.all()
-    return {"data": records_schema.dumps(records)}
+@bp.route("/records/", methods=['GET'])
+@use_kwargs({
+    "states": fields.DelimitedList(fields.Str()),
+    "hrsa_designations": fields.DelimitedList(fields.Str()),
+    "years": fields.DelimitedList(fields.Str())
+}, location='query')
+def records(states=None, hrsa_designations=None, years=None):
+    query = Record.query
+    if (states):
+        query = query.filter(Record.state.in_(states))
+
+    if (hrsa_designations):
+        query = query.filter(Record.entity_abv.in_(hrsa_designations))
+    if (years):
+        query = query.filter(Record.full_year.in_(years))
+    return records_schema.dumps(query.order_by('year').all())
+
+@bp.route("/states/")
+def states():
+    states = Record.query.with_entities(Record.state).distinct().all()
+    return jsonify(list(chain(*states)))
 
 
 @bp.route("/summary")
