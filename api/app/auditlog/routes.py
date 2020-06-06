2450836572fbbcf3be3034066564b4ba0f29db50
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-06-05 18:56:59 -0700

    Add entity and findings searchable fields

--- api/app/auditlog/routes.py
+++ api/app/auditlog/routes.py
@@ -14,9 +14,11 @@ from .schemas import record_schema, records_schema
 @use_kwargs({
     "states": fields.DelimitedList(fields.Str()),
     "hrsa_designations": fields.DelimitedList(fields.Str()),
-    "years": fields.DelimitedList(fields.Str())
+    "years": fields.DelimitedList(fields.Str()),
+    "findings_keywords": fields.Str(),
+    "entity_keywords": fields.Str()
 }, location='query')
-def records(states=None, hrsa_designations=None, years=None):
+def records(states=None, hrsa_designations=None, years=None, findings_keywords=None, entity_keywords=None):
     query = Record.query
     if (states):
         query = query.filter(Record.state.in_(states))
@@ -25,7 +27,12 @@ def records(states=None, hrsa_designations=None, years=None):
         query = query.filter(Record.entity_abv.in_(hrsa_designations))
     if (years):
         query = query.filter(Record.full_year.in_(years))
-    return records_schema.dumps(query.order_by('year').all())
+    if (findings_keywords):
+        query = query.filter(Record.opa_findings.like('%' + findings_keywords + '%'))
+    if (entity_keywords):
+        print(entity_keywords)
+        query = query.filter(Record.entity.like('%' + entity_keywords + '%'))
+    return records_schema.dumps(query.all())
 
 @bp.route("/states/")
 def states():
