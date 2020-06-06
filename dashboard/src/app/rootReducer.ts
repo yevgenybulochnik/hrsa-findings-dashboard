c26e0fbe1613ad22ddfc49edc658c7c9d187aba8
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-06-06 11:55:49 -0700

    Include search reducers in rootReducer

--- dashboard/src/app/rootReducer.ts
+++ dashboard/src/app/rootReducer.ts
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux'
 import { auditData, selectedAuditEntry }from './ducks/reducers'
 import filterReducer from './components/filterCard/ducks/reducers'
+import searchReducer from './components/searchCard/ducks/reducers'
 
 
 const rootReducer = combineReducers({
   auditData,
   selectedAuditEntry,
   filters: filterReducer,
+  search: searchReducer,
 })
 
 export type RootState = ReturnType<typeof rootReducer>
