79df74bc0ebd3a902f14bacffa1bb7b159fcd393
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-06-20 12:05:39 -0700

    Flask migrate plugin setup

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
 from config import Config
 from app.database import db
 
 ma = Marshmallow()
+migrate = Migrate()
 
 
 def create_app(config=Config):
@@ -12,11 +14,9 @@ def create_app(config=Config):
 
     db.init_app(app)
     ma.init_app(ma)
+    migrate.init_app(app, db)
 
     from . import auditlog
     app.register_blueprint(auditlog.bp, url_prefix='/api')
-
-    with app.app_context():
-        db.create_all()
 
     return app
