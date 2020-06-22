5f250b75f4bc3b7c64f7de49932987c9300461fd
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-06-21 20:11:57 -0700

    State, HrsaDesignation models and schemas

--- api/app/auditlog/models.py
+++ api/app/auditlog/models.py
@@ -1,4 +1,21 @@
-from app.database import db, Model, Column
+from app.database import db, Model, Column, relationship
+
+
+class State(Model):
+    __tablename__ = 'states'
+    id = Column(db.Integer, primary_key=True)
+    abv = Column(db.String())
+    name = Column(db.String())
+    color = Column(db.String())
+    records = relationship('Record', backref='state', lazy='dynamic')
+
+
+class HrsaDesignation(Model):
+    __tablename__ = 'hrsa_designations'
+    id = Column(db.Integer, primary_key=True)
+    designation = Column(db.String())
+    abv = Column(db.String())
+    records = relationship('Record', backref='hrsa_des', lazy='dynamic')
 
 
 class Record(Model):
@@ -6,8 +23,8 @@ class Record(Model):
     id = Column(db.Integer, primary_key=True)
     hrsa_id = Column(db.String())
     entity = Column(db.String())
-    entity_abv = Column(db.String())
-    state = Column(db.String())
+    hrsa_designation_id = Column(db.Integer, db.ForeignKey('hrsa_designations.id'))
+    state_id = Column(db.Integer, db.ForeignKey('states.id'))
     year = Column(db.String())
     full_year = Column(db.Integer)
     opa_findings = Column(db.String())
--- api/app/auditlog/schemas.py
+++ api/app/auditlog/schemas.py
@@ -1,8 +1,18 @@
 from app import ma
-from .models import Record
+from .models import Record, State, HrsaDesignation
 
+
+class HrsaDesignationSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = HrsaDesignation
+
+class StateSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = State
 
 class RecordSchema(ma.SQLAlchemyAutoSchema):
+    state = ma.Pluck(StateSchema, 'abv')
+    hrsa_des = ma.Pluck(HrsaDesignationSchema, 'abv')
     class Meta:
         model = Record
 
