b969eb1b0e29b850d179d0fc9e8611af0dea18fb
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-06-28 16:01:19 -0700

    Add summary endpoint

--- dashboard/src/app/services/dataService.ts
+++ dashboard/src/app/services/dataService.ts
@@ -11,14 +11,14 @@ type RecordQueryParams = {
 
 function getRecords(queryParams: RecordQueryParams) {
   let url = '/api/records/'
-  let queryString = '?'
+  let queryStrings = []
   if (queryParams) {
     for (const key in queryParams) {
       if (queryParams[key].length) {
-        queryString += `${key}=${queryParams[key]}`
+        queryStrings.push(`${key}=${queryParams[key]}`)
       }
     }
-    url += queryString
+    url += `?${queryStrings.join('&')}`
   }
   return fetch(url)
     .then((res) => res.json())
@@ -29,9 +29,30 @@ function getFilterItems() {
     .then((res) => res.json())
 }
 
+type SummaryQueryParams = {
+  states: string[];
+  [ key: string ]: any;
+}
+
+function getSummary(queryParams: SummaryQueryParams) {
+  let url = '/api/summary/'
+  let queryStrings = []
+  if (queryParams) {
+    for (const key in queryParams) {
+      if (queryParams[key].length) {
+        queryStrings.push(`${key}=${queryParams[key]}`)
+      }
+    }
+    url += `?${queryStrings.join('&')}`
+  }
+  return fetch(url)
+    .then((res) => res.json())
+}
+
 const dataService = {
   getRecords,
   getFilterItems,
+  getSummary,
 }
 
 export default dataService
