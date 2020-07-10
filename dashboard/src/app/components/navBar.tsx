3601aaeaeb2a4c2297a22bd01b3c99fe21da26a9
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-07-10 08:04:46 -0700

    Mobile nav bar

--- dashboard/src/app/components/navBar.tsx
+++ dashboard/src/app/components/navBar.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Link, RouteComponentProps, withRouter } from 'react-router-dom'
-import { Tabs, Tab, Navbar } from '@blueprintjs/core'
+import { Tabs, Tab, Navbar, Button, Popover, Menu, MenuItem } from '@blueprintjs/core'
+
+const Styles = styled.div`
+  .nav-tabs-desktop {
+    display: None;
+  }
+  .nav-tabs-mobile {
+  }
+  @media(min-width: 750px) {
+    .nav-tabs-desktop {
+      display: block;
+    }
+    .nav-tabs-mobile {
+      display: None;
+    }
+  }
+`
 
 const Spacer = styled.span`
   width: 50px;
@@ -16,7 +32,8 @@ const Nav: React.SFC<Props> = (props) => {
   const {
     branding,
     navLinks,
-    location
+    location,
+    history
   } = props
 
   const setPathname = () => {
@@ -27,24 +44,43 @@ const Nav: React.SFC<Props> = (props) => {
   }
 
   return (
-    <Navbar>
-      <Navbar.Group>
-        <Navbar.Heading>{branding}</Navbar.Heading>
-        <Navbar.Divider />
-      </Navbar.Group>
-      <Navbar.Group>
-        <Tabs selectedTabId={setPathname()} large={true} animate={true}>
-          <Spacer />
-          {navLinks.map(navlink => (
-            <Tab
-              key={navlink.id}
-              id={navlink.path}
-              title={<Link to={navlink.path}>{ navlink.name }</Link>}
-            />
-          ))}
-        </Tabs>
-      </Navbar.Group>
-    </Navbar>
+    <Styles>
+      <Navbar>
+        <Navbar.Group>
+          <Navbar.Heading>{branding}</Navbar.Heading>
+          <Navbar.Divider />
+        </Navbar.Group>
+        <Navbar.Group className='nav-tabs-desktop'>
+          <Tabs selectedTabId={setPathname()} large={true} animate={true}>
+            <Spacer />
+            {navLinks.map(navlink => (
+              <Tab
+                key={navlink.id}
+                id={navlink.path}
+                title={<Link to={navlink.path}>{ navlink.name }</Link>}
+              />
+            ))}
+          </Tabs>
+        </Navbar.Group>
+        <Navbar.Group className='nav-tabs-mobile' align='right'>
+          <Popover
+            content={
+              <Menu>
+                {navLinks.map((navlink, id: number) => (
+                  <MenuItem
+                    key={`link-${id}`}
+                    onClick={() => history.push(navlink.path)}
+                    text={navlink.name}
+                  />
+                ))}
+              </Menu>
+            }
+          >
+            <Button>Menu</Button>
+          </Popover>
+        </Navbar.Group>
+      </Navbar>
+    </Styles>
   )
 }
 
