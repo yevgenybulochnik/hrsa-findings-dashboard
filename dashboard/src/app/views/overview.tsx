921ccec18417cfa2a7c15d9ec6a23a9144251bd0
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-08-15 09:40:32 -0700

    Adjust overview layout

--- dashboard/src/app/views/overview.tsx
+++ dashboard/src/app/views/overview.tsx
@@ -4,9 +4,46 @@ import styled from 'styled-components'
 import { SummaryChart, SummaryPie, FilterCard } from '../components/summaryChart'
 import { RootState } from '../rootReducer'
 
+const SideBar = styled.div``
+
+const PieContainer = styled.div``
+
+const BarChartContainer = styled.div``
+
+
 const Container = styled.div`
-  > * {
-    margin-bottom: 1em;
+  ${SideBar}, ${PieContainer}, ${BarChartContainer} {
+    > * {
+      margin-bottom: 1em;
+    }
+  }
+
+  @media(min-width: 750px) {
+    margin: 1em;
+    display: grid;
+    grid-template-columns: 3fr 8fr;
+    grid-template-rows: auto;
+    grid-template-areas:
+      "sidebar piecharts"
+      "barcharts barcharts";
+
+    ${SideBar} {
+      grid-area: sidebar;
+      padding-top: 3em;
+    }
+
+    ${PieContainer} {
+      > * {
+        width: 500px;
+      }
+      justify-content: space-around;
+      display: flex;
+      grid-area: piecharts;
+    }
+
+    ${BarChartContainer} {
+      grid-area: barcharts;
+    }
   }
 `
 
@@ -23,10 +60,16 @@ const Overview: React.SFC<Props> = (props) => {
 
   return (
     <Container>
-      <FilterCard />
-      <SummaryChart />
-      <SummaryPie data={pieData1} />
-      <SummaryPie data={pieData2} />
+      <SideBar>
+        <FilterCard />
+      </SideBar>
+      <BarChartContainer>
+        <SummaryChart />
+      </BarChartContainer>
+      <PieContainer>
+        <SummaryPie data={pieData1} />
+        <SummaryPie data={pieData2} />
+      </PieContainer>
     </Container>
   )
 }
