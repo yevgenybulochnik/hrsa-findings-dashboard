682c6275450241cc1ce7e3223ca3590acc67c672
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-08-15 10:36:29 -0700

    Adjust pichart text, size, width

--- dashboard/src/app/components/summaryChart/summaryPieChart.tsx
+++ dashboard/src/app/components/summaryChart/summaryPieChart.tsx
@@ -9,17 +9,17 @@ const renderActiveShape = (props: any) => {
     fill, payload, percent, value } = props;
   const sin = Math.sin(-RADIAN * midAngle);
   const cos = Math.cos(-RADIAN * midAngle);
-  const sx = cx + (outerRadius + 10) * cos;
-  const sy = cy + (outerRadius + 10) * sin;
-  const mx = cx + (outerRadius + 30) * cos;
-  const my = cy + (outerRadius + 30) * sin;
+  const sx = cx + (outerRadius + 2.5) * cos;
+  const sy = cy + (outerRadius + 2.5) * sin;
+  const mx = cx + (outerRadius + 7.5) * cos;
+  const my = cy + (outerRadius + 7.5) * sin;
   const ex = mx + (cos >= 0 ? 1 : -1) * 22;
   const ey = my;
   const textAnchor = cos >= 0 ? 'start' : 'end';
 
   return (
     <g>
-      <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>{payload.name}</text>
+      <text fontSize="smaller" x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>{payload.name}</text>
       <Sector
         cx={cx}
         cy={cy}
@@ -40,7 +40,7 @@ const renderActiveShape = (props: any) => {
       />
       <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none"/>
       <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none"/>
-      <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">{`Records: ${value}`}</text>
+      <text fontSize="smaller" x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">{`Records: ${value}`}</text>
       <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
         {`${(percent * 100).toFixed(2)}%`}
       </text>
@@ -67,8 +67,8 @@ const SummaryPieChart: React.SFC<Props> = (props) => {
             dataKey='value'
             nameKey='name'
             fill="#82ca9d"
-            innerRadius='40%'
-            outerRadius='60%'
+            innerRadius='35%'
+            outerRadius='50%'
             onMouseEnter={(_, index) => setActiveIndex(index)}
           >
             {
