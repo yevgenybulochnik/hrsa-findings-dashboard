2f5b8f40d2227981faa529eac9fa5584fdb3b635
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-08-18 21:05:03 -0700

    Testing config setup

--- api/config.py
+++ api/config.py
@@ -7,3 +7,7 @@ basedir = Path(__file__).parent
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI', f'sqlite:///{basedir}/hrsa.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class TestConfig(object):
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@db:5432/test_hrsa"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
